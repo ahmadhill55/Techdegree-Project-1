adbe70ed2929b15067da5c211adeaae23584599f
ahmadhill55 <79861596+ahmadhill55@users.noreply.github.com>
2021-03-07 11:13:42 -0800

    Added all quotes

---
+++ js/script.js
@@ -0,0 +1,54 @@
+/******************************************
+Treehouse FSJS Techdegree:
+project 1 - A Random Quote Generator
+******************************************/
+
+// For assistance: 
+// Check the "Project Resources" section of the project instructions
+// Reach out in your Slack community - https://treehouse-fsjs-102.slack.com/app_redirect?channel=chit-chat
+
+/*** 
+ * `quotes` array 
+***/
+let quotes = [
+  {
+    quote: 'Simplicity is the ultimate sophistication.', name: 'Leonardo da Vinci'
+  },
+
+  {
+    quote: 'What we think, we become.', name: 'Buddha'
+  },
+
+  {
+    quote: 'Tough times never last but tough people do.', name: 'Robert H. Schiuller'
+  },
+
+  {
+    quote: 'Happiness depends upon ourselves.', name: 'Aristotles'
+  },
+
+  {
+    quote: 'Die with memories, not dreams.', name: 'Unknown'
+  }
+
+];
+
+
+/***
+ * `getRandomQuote` function
+***/
+
+
+
+/***
+ * `printQuote` function
+***/
+
+
+
+/***
+ * click event listener for the print quote button
+ * DO NOT CHANGE THE CODE BELOW!!
+***/
+
+document.getElementById('load-quote').addEventListener("click", printQuote, false);
